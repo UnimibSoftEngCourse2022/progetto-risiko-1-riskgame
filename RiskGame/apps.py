2179186a2c8b43aa3f54906f26e6da0e3e30e3b4
Kelarch <l.calarco@campus.unimib.it>
2022-01-10 11:32:07 +0100

    Prima commit

---
+++ RiskGame/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RiskgameConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'RiskGame'
---
+++ RiskGame/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+
+# Create your views here.
+
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
