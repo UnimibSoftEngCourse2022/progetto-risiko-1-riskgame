4f60e0035af74b7d213ad64725741ec09511a8a5
Kelarch <l.calarco@campus.unimib.it>
2022-01-12 12:22:22 +0100

    Aggiunta schermata Registrazione e Menù

--- RiskGame/urls.py
+++ RiskGame/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import HomePageView, LoginView
+from .views import HomePageView, LoginView, RegistrazioneView, MenuView
 
 urlpatterns = [
-    path("", HomePageView.as_view(), name = "home"),
-    path("login", LoginView.as_view(), name = "login"),
+    path("", HomePageView.as_view(), name="home"),
+    path("login", LoginView.as_view(), name="login"),
+    path("registrazione", RegistrazioneView.as_view(), name="registrazione"),
+    path("menu", MenuView.as_view(), name="menu"),
 ]
--- RiskGame/views.py
+++ RiskGame/views.py
@@ -8,5 +8,14 @@ from django.views.generic import TemplateView
 class HomePageView(TemplateView):
     template_name = "home.html"
 
+
 class LoginView(TemplateView):
     template_name = "login.html"
+
+
+class RegistrazioneView(TemplateView):
+    template_name = "registrazione.html"
+
+
+class MenuView(TemplateView):
+    template_name = "menu.html"
