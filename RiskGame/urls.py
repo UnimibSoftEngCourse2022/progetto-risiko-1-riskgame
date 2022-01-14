1db6d9f7a7ca50175c536e8f593025e0fdac5adb
Kelarch <l.calarco@campus.unimib.it>
2022-01-14 10:30:20 +0100

    Aggiunte le ultime schermate che non richiedono l'uso del database

--- RiskGame/urls.py
+++ RiskGame/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import HomePageView, LoginView, RegistrazioneView, MenuView
+from .views import HomePageView, LoginView, RegistrazioneView, MenuView, CreazioneView, ImpostazioniView
+from . import views
+app_name = "RiskGame"
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("login", LoginView.as_view(), name="login"),
     path("registrazione", RegistrazioneView.as_view(), name="registrazione"),
     path("menu", MenuView.as_view(), name="menu"),
+    path("crea-partita", CreazioneView.as_view(), name="creazione"),
+    path("settings", ImpostazioniView.as_view(), name="impostazione"),
+    path("registrazione-salvataggio", views.saveUserData, name="saveUserData"),
+
 ]
--- RiskGame/views.py
+++ RiskGame/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from . import models
 
 
 # Create your views here.
@@ -17,5 +18,34 @@ class RegistrazioneView(TemplateView):
     template_name = "registrazione.html"
 
 
+
+
 class MenuView(TemplateView):
     template_name = "menu.html"
+
+
+class ImpostazioniView(TemplateView):
+    template_name = "impostazione.html"
+
+
+class CreazioneView(TemplateView):
+    template_name = "creazione.html"
+
+    def loadMappe(self, request):
+        if request.user.is_authenticated():
+            nickname = request.user.username
+            mappe = Mappa.objects.filter(Autore=nickname)
+            return render(request, self.template_name, {'mappe': mappe})
+
+
+def saveUserData(request):
+        if request.method == "POST":
+            NickName = request.POST['nickname']
+            Nome = request.POST['nome']
+            Cognome = request.POST['cognome']
+            Email = request.POST['email']
+            Password = request.POST['password']
+            GiocatoreRegistrato.objects.create(Nome=Nome, Cognome=Cognome, NickName=NickName, Email=Email,
+                                               Password=Password)
+            messages.success(request, 'I dati sono stati salvati')
+        return render(request, 'registrazione.html')
