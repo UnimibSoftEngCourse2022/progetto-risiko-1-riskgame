e4f1a47e6f6979baa4a5dfd1dc971cf7b13d462f
Ismail El Abiad <ismail.elabiad1998@gmail.com>
2022-01-19 10:30:15 +0100

    modifica

--- RiskGame/views.py
+++ RiskGame/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
-from . import models
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
 
